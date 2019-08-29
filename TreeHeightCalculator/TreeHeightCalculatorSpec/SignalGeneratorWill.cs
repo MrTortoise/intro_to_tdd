a554edf718de56cfe06ccd31ce258b61b21b50a0
John Nicholas <john.nicholas@summize.it>
2019-08-29 09:59:59 +0100

    time to add a nwe tset ... but first refactor

--- TreeHeightCalculator/TreeHeightCalculatorSpec/SignalGeneratorWill.cs
+++ TreeHeightCalculator/TreeHeightCalculatorSpec/SignalGeneratorWill.cs
@@ -10,8 +10,34 @@ namespace TreeHeightCalculatorSpec
         [Fact]
         public void ProduceTheRightSetOfData()
         {
-            Assert.Equal(true,false);
+            var expectedresults = new List<string>()
+            {
+                "1",
+                "2",
+                "3",
+                "connifer",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "fungus",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "19",
+                "20"
+            };
 
+            Assert.Equal(expectedresults, expectedresults);
+            Assert.Equal(22, expectedresults.Count);
         }
     }
 }
